e8b0eca62f3dbd2e2a29b430699a689561ce6ef3
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2023-12-25 13:52:22 +0500

    change code

--- src/utils/models.py
+++ src/utils/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class State(models.Model):
+    name = models.CharField(max_length=255)
+    attr = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'States'
+        verbose_name = 'State'
+        db_table = 'states'
+
+    def save(self, *args, **kwargs):
+        self.updated_at = timezone.now()
+        super(State, self).save(*args, **kwargs)
+        return self
