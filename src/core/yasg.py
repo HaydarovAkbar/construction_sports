724df4d069c5d8e0e6be09d235a07505ee9c4b2b
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2023-12-28 14:29:23 +0500

    change core code

--- src/core/yasg.py
+++ src/core/yasg.py
@@ -1,8 +1,5 @@
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 from rest_framework import permissions
 from django.conf import settings
 
