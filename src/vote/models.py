d8ff95527c29b247de01fcbf00ceb50d29d73694
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2023-12-29 09:37:12 +0500

    change code

--- src/vote/models.py
+++ src/vote/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from utils.models import State, Region, District, Neighborhood
+from accounts.models import User
 
-# Create your models here.
+
+class Votes(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Ovozlar')
+    attr = models.CharField(max_length=255, verbose_name='Atribut', null=True, blank=True)
+
+    state = models.ForeignKey(State, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Голосования'
+        verbose_name = 'Голосование'
+        db_table = 'votes'
