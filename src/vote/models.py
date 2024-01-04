3c5a9b468812f7e33e998e7cd6fc95a9fec32bc7
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2024-01-04 20:54:40 +0500

    change votes

--- src/vote/models.py
+++ src/vote/models.py
@@ -3,14 +3,16 @@ from utils.models import State, Region, District, Neighborhood
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from account.models import User
+from app.models import Construction
 
 
 class Votes(models.Model):
-    title = models.CharField(max_length=255, verbose_name=_('Ovozlar'))
-    attr = models.CharField(max_length=255, verbose_name=_('Atribut'), null=True, blank=True)
-
-    state = models.ForeignKey(State, on_delete=models.SET_NULL, null=True)
+    state = models.ForeignKey(State, on_delete=models.SET_NULL, null=True, blank=True, verbose_name=_('Состояние'))
     created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, verbose_name=_('Пользователь'))
+    construction = models.ForeignKey(Construction, on_delete=models.SET_NULL, null=True, blank=True,
+                                     verbose_name=_('Строительство'))
 
     def __str__(self):
         return self.title
