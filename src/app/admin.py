32378d486919fa9a363c49bcfbe8af518009a00d
Akbar Haydarov <81344759+HaydarovAkbar@users.noreply.github.com>
2024-01-06 12:03:59 +0500

    .

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ConstructionType, Season, Basis, WhereIsBuilt, Construction, ConstructionImage
+from .models import ConstructionType, Season, Basis, WhereIsBuilt, Construction, ConstructionImage, SeasonStat
 
 admin.site.register(ConstructionType)
 admin.site.register(Season)
@@ -7,3 +7,7 @@ admin.site.register(Basis)
 admin.site.register(WhereIsBuilt)
 admin.site.register(Construction)
 admin.site.register(ConstructionImage)
+admin.site.site_header = 'Qurilish sport Admin panel'
+admin.site.site_title = 'Qurilish sport Admin panel'
+admin.site.index_title = 'Qurilish sport Admin panel'
+admin.site.register(SeasonStat)
